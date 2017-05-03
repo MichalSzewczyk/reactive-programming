410707ad26b8874d0a3cbdfd4e683016e00377ea
mszewczyk <szewczykm94@gmail.com>
2017-05-03 18:16:06 +0200

    Created Flowable basic example

--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -1,3 +1,4 @@
+import io.reactivex.Flowable;
 import org.junit.Assert;
 import org.junit.Test;
 import io.reactivex.Observable;
@@ -9,9 +10,27 @@ import io.reactivex.Observable;
 public class Examples {
     private final static String TEST = "test";
     private boolean receivedTest;
+
+    /**
+     * First, basic usage
+     */
     @Test
     public void checkIfSubscriberReceivedTestValue(){
+        receivedTest = false;
         Observable<String> observable = Observable.just(TEST);
+        observable.subscribe(string -> receivedTest = string.equals(TEST));
+        Assert.assertTrue(receivedTest);
+    }
+
+    /**
+     * Flowable<T> example
+     * Flowable - emits 0 or n items and terminates with complete or an error.
+     * Supports backpressure, which allows to control how fast a source emits items.
+     */
+    @Test
+    public void checkIfSubscriberReceivedTestValueFromFlowable(){
+        receivedTest = false;
+        Flowable<String> observable = Flowable.just(TEST);
         observable.subscribe(string -> receivedTest = string.equals(TEST));
         Assert.assertTrue(receivedTest);
     }
