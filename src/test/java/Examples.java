10902371e78bf0cc4eb86367665c38b00224b53e
mszewczyk <szewczykm94@gmail.com>
2017-05-03 18:18:25 +0200

    Created Single basic example

--- src/test/java/Examples.java
+++ src/test/java/Examples.java
@@ -1,7 +1,8 @@
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import org.junit.Assert;
 import org.junit.Test;
-import io.reactivex.Observable;
 
 /**
  * Created by Michał Szewczyk on 2017-05-03.
@@ -31,6 +32,20 @@ public class Examples {
     public void checkIfSubscriberReceivedTestValueFromFlowable(){
         receivedTest = false;
         Flowable<String> observable = Flowable.just(TEST);
+        observable.subscribe(string -> receivedTest = string.equals(TEST));
+        Assert.assertTrue(receivedTest);
+    }
+
+    /**
+     * Single<T> example
+     * Single - Emits either a single item or an error.
+     * The reactive version of a method call.
+     * You subscribe to a Single and you get either a return value or an error.
+     */
+    @Test
+    public void checkIfSubscriberReceivedTestValueFromSingle() {
+        receivedTest = false;
+        Single<String> observable = Single.just(TEST);
         observable.subscribe(string -> receivedTest = string.equals(TEST));
         Assert.assertTrue(receivedTest);
     }
